20a6751638167c8b09a79873b9e8abd97d94fd76
Tushar Budhwani <tusharbudhwani@gmail.com>
2023-11-26 01:06:19 -0500

    Write scripts for AssignEmployee Buttonand datatable

--- assignshift.php
+++ assignshift.php
@@ -14,6 +14,13 @@ if (!isset($_SESSION['email'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+      <!-- Include toastr CSS file -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+
+<!-- Include toastr JS file -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
 </head>
 
 <body>
@@ -98,59 +105,91 @@ if (!isset($_SESSION['email'])) {
         <div class="row text-white">
             <div class="row text-white">
                 <div class="col-lg-3">
-                    <button type="button" class="btn btn-white" onclick="showTable('line')">Line</button>
+                    <button type="button" class="btn btn-white" onclick="showTable('1')">Line</button>
                 </div>
                 <div class="col-lg-3">
-                    <button type="button" class="btn btn-white" onclick="showTable('dishroom')">Dishroom</button>
+                    <button type="button" class="btn btn-white" onclick="showTable('2')">Dishroom</button>
                 </div>
                 <div class="col-lg-3">
-                    <button type="button" class="btn btn-white" onclick="showTable('kitchen')">Kitchen</button>
+                    <button type="button" class="btn btn-white" onclick="showTable('3')">Kitchen</button>
                 </div>
                 <div class="col-lg-3">
-                    <button type="button" class="btn btn-white" onclick="showTable('grabngo')">GrabnGo</button>
+                    <button type="button" class="btn btn-white" onclick="showTable('4')">GrabnGo</button>
                 </div>
 
 
                 <div class="table-content active-content" id="line-content">
                     <!-- Table for Line -->
                     <div class="table-responsive">
-                        <table class="table table-bordered table-dark">
-                            <!-- Add your table content here -->
+                        <table class="table table-bordered table-dark" id="myDataTable">
+                        <!-- <table class="table text-white table-border"> -->
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Sub Category </th>
+                    <th>Time Slot </th>
+                    <th>Last Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+           <tbody></tbody>
+        </table>
                         </table>
                     </div>
                 </div>
 
-                <div class="table-content" id="dishroom-content">
-                    <!-- Table for Dishroom -->
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-dark">
-                            <!-- Add your table content here -->
-                        </table>
-                    </div>
-                </div>
 
-                <div class="table-content" id="kitchen-content">
-                    <!-- Table for Kitchen -->
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-dark">
-                            <!-- Add your table content here -->
-                        </table>
-                    </div>
-                </div>
-
-                <div class="table-content" id="grabngo-content">
-                    <!-- Table for Grab N' Go -->
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-dark">
-                            <!-- Add your table content here -->
-                        </table>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="footer-copyright text-center text-light py-3 mt-5">&copy; Copyright DCSMS</div>
 
     </div>
+
+    <div class="modal" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Assign Employee</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Modal Body -->
+            <form method="post">
+            <div class="modal-body">
+
+            <div class="form-group row">
+                        <label class="col-sm-12 col-form-label">Assign Employee</label>
+                        <input type="hidden" name="schid" id="schid" value="">
+                        <div class="form-group">
+                            <select class="form-control" id="AssignEmployee" name="AssignEmployee">
+                                <?php
+                $role = 1;
+                $query = "SELECT * FROM staff WHERE role='$role'";
+                $query_run = mysqli_query($conn, $query);
+                $sno = 0;
+                while ($row = mysqli_fetch_assoc($query_run)) {
+
+                    ?>
+                    <option value='<?php echo $row['sid']; ?>'><?php echo $row['fname']; ?> </option>
+                <?php
+                }
+                ?>
+                            </select>
+                        </div>
+                    </div>
+            </div>
+
+            <!-- Modal Footer -->
+            <div class="modal-footer">
+                <button type="button" id="AssignEmployeebtn" class="btn btn-primary">Assign Role</button>
+            </div>
+            </form>''
+
+        </div>
+    </div>
+</div>
 
 
 
@@ -158,9 +197,67 @@ if (!isset($_SESSION['email'])) {
     <!-- End demo content -->
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        function AssignId(data){
+            $('#schid').val(data);
+            $('#myModal').modal('show');
+        }
+        $('#AssignEmployeebtn').on('click',function(){
+            var schid=$('#schid').val();
+            var AssignEmp=$('#AssignEmployee').val();
+            $.ajax({
+                type:'post',
+                url:'http://localhost/project-dcsms/api/AssignTaskEmp.php',
+                data:{
+                    AssignEmp:AssignEmp,
+                    schid:schid,
+                },
+                success: function(responsedata){
+                    var response=JSON.parse(responsedata);
+                    console.log(response);
+                    if(response.status.trim()=="success"){
+                        toastr.success("Succesfully Added Employee");
+                        $('#myModal').modal('hide');
+                        return ;
+                        $(document.querySelector('form')).reset();
+                    }else{
+                        toastr.error("Please contact Developer");
+                        $('#myModal').modal('hide');
+                    }
+                }
+            })
+        })
+      function showTable(data) {
+        $('#myDataTable').DataTable().destroy();
+    $('#myDataTable').DataTable({
+        "ajax": {
+            "url": 'http://localhost/project-dcsms/api/getassignshift.php',
+            "type": "POST",
+            "data": function () {
+                // You can send parameters here if needed
+                return {
+                    CatId: data
+                };
+            }
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "subcategory" },
+            { "data": "day" },
+            { "data": "timeslot" },
+            { "data": "action" },
+        ]
+    });
+}
+
+    </script>
 </body>
 
 </html>
