88467d77554b05f805774baadbad5a3fd347ce2b
Tushar Budhwani <tusharbudhwani@gmail.com>
2023-11-28 21:33:19 -0500

    Delete deletestaff.php
    API call is now done from API folder.

--- deletestaff.php
+++
@@ -1,9 +0,0 @@
-<?php
-include './config/connect.php';
-$sql = "DELETE FROM staff WHERE sid='" . $_GET["sid"] . "'";
-if (mysqli_query($conn, $sql)) {
-    echo "<script>window.location='managestaff.php'</script>";
-} else {
-    echo "<script>alert('Failed to delete!');window.location='managestaff.php'</script>";
-    header("location:managestaff.php");
-}
