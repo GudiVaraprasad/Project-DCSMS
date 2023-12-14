2275a611acaa7adac1141fe3d7a5f393dfc9dc7e
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2023-12-13 23:39:35 -0500

    Fix bug in AssignShift ajax call

--- backend/api/getassignshift.php
+++ backend/api/getassignshift.php
@@ -3,10 +3,8 @@ include('../config/connect.php');
 if (isset($_POST['CatId'])) {
     $cid = $_POST['CatId'];
     $curr_day = date('l');
-    // echo $curr_day;
-    $sql = "SELECT schid,day,starttime,endtime,sid,schedule.scid,scname FROM schedule LEFT JOIN subcategory on schedule.scid=subcategory.scid WHERE schedule.cid='$cid'";
+    $sql = "SELECT schid,day,starttime,endtime,sid,schedule.scid,scname FROM schedule LEFT JOIN subcategory on schedule.scid=subcategory.scid WHERE schedule.cid='$cid' AND schedule.sid is NULL";
     $result = $conn->query($sql);
-    // print_r($sql);
     $data = array();
     if ($result->num_rows > 0) {
         $count = 1;
@@ -17,7 +15,7 @@ if (isset($_POST['CatId'])) {
                 $tempdata['subcategory'] = $row['scname'];
                 $tempdata['day'] = $row['day'];
                 $tempdata['timeslot'] = $row['starttime'] . "-" . $row['endtime'];
-                $tempdata['action'] = "<button class='btn btn-white' onclick='AssignId(" . $row['schid'] . ")'>Assign</button>";
+                $tempdata['action'] = "<button class='btn btn-white' onclick='AssignId(" . $row['schid'] . ',' . $row['scid'] . ")'>Assign</button>";
                 $data[] = $tempdata;
             }
         }
