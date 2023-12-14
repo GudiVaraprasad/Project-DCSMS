814468f8f98894a5dd2537b98577cbfc5de6a39e
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2023-12-14 02:41:15 -0500

    Enhanced password storage mechanism for improved security

--- frontend/pages/managerlogin.php
+++ frontend/pages/managerlogin.php
@@ -31,7 +31,7 @@ include '../../backend/config/connect.php';
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $pass = $_POST['pass'];
-    $query = "SELECT * FROM staff WHERE email='$email' AND password='$pass' AND role=0";
+    $query = "SELECT * FROM staff WHERE email='$email' AND password=PASSWORD('$pass') AND role=0";
     $result = mysqli_query($conn, $query) or die($mysqli_error($conn));
     $num = mysqli_num_rows($result);
     if ($num == 0) {
