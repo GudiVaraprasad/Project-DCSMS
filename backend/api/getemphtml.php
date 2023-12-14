bf8443f32d5fc0ac08e3c7d96b94420224c6dc62
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2023-12-13 23:40:22 -0500

    Fix bug in modal for assign call

--- backend/api/getemphtml.php
+++ backend/api/getemphtml.php
@@ -1,25 +1,26 @@
 <?php
 include('../config/connect.php');
+$curr_day = date('l');
 if (isset($_POST['schid'])) {
     $schid = $_POST['schid'];
     $sql = "SELECT *
     FROM staff
     WHERE sid NOT IN (
         SELECT staffshift.sid
-        FROM staffshift INNER JOIN schedule on staffshift.scid=schedule.sid
-        WHERE schedule.schid!='$schid'
+        FROM staffshift INNER JOIN schedule on staffshift.scid=schedule.scid
+        WHERE schedule.day = '$curr_day'
     );
     ";
     $result = $conn->query($sql);
 
-    $html='<select class="form-control" id="AssignEmployee" name="AssignEmployee">';
+    $html = '<select class="form-control" id="AssignEmployee" name="AssignEmployee">';
 
     if ($result->num_rows > 0) {
         $count = 1;
         while ($row = $result->fetch_assoc()) {
-           $html.='<option value='.$row['sid'].'>'. $row['fname'].'  </option>';
+            $html .= '<option value=' . $row['sid'] . '>' . $row['fname'] . '  </option>';
         }
     }
-    $html.='</select>';
+    $html .= '</select>';
     echo $html;
 }
